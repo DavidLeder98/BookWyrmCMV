2f3ebcd8f000ae2dd2db31845d8f9fc409014025
David Leder <davidleder98@gmail.com>
2024-08-27 21:23:04 +0200

    connection string not working

--- BookWyrmCMV/Program.cs
+++ BookWyrmCMV/Program.cs
@@ -1,3 +1,6 @@
+using BookWyrmCMV.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookWyrmCMV
 {
     public class Program
@@ -8,6 +11,7 @@ namespace BookWyrmCMV
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
