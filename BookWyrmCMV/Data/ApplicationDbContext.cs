d056567ea3af6db0e8032dc5c5d362ec2093c378
David Leder <davidleder98@gmail.com>
2024-08-27 21:10:01 +0200

    app db context

---
+++ BookWyrmCMV/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookWyrmCMV.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+    }
+}
