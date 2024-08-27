af22b4c2f6fd69da59de1707f4e54793b90b1ae2
David Leder <davidleder98@gmail.com>
2024-08-27 18:59:56 +0200

    Add project files.

---
+++ BookWyrmCMV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookWyrmCMV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
