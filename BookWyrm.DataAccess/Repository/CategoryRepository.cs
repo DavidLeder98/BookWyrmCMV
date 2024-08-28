dbbe3e5acc3c92b199b677b019b84edad3f7bf3c
David Leder <davidleder98@gmail.com>
2024-08-28 21:50:16 +0200

    category repositore for update and save

---
+++ BookWyrm.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookWyrm.DataAccess.Data;
+using BookWyrm.DataAccess.Repository.IRepository;
+using BookWyrm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWyrm.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<CategoryModel>, ICategoryRepository
+	{
+		private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+		{
+			_db.SaveChanges();
+		}
+
+		public void Update(CategoryModel obj)
+		{
+			_db.Categories.Update(obj);
+		}
+	}
+}
---
+++ BookWyrm.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookWyrm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWyrm.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<CategoryModel>
+	{
+		void Update(CategoryModel obj);
+		void Save();
+	}
+}
