c94c7195f50bbbe45a886a2e873d8a81c4256bc0
David Leder <davidleder98@gmail.com>
2024-08-28 18:07:30 +0200

    Toastr notifications

--- BookWyrmCMV/Controllers/CategoryController.cs
+++ BookWyrmCMV/Controllers/CategoryController.cs
@@ -30,6 +30,7 @@ namespace BookWyrmCMV.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+				TempData["success"] = "Category created successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -57,6 +58,7 @@ namespace BookWyrmCMV.Controllers
 			{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully.";
 				return RedirectToAction("Index");
 			}
 			return View();
@@ -87,6 +89,7 @@ namespace BookWyrmCMV.Controllers
 			}
 			_db.Categories.Remove(obj);
 			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully.";
 			return RedirectToAction("Index");
 		}
 	}
