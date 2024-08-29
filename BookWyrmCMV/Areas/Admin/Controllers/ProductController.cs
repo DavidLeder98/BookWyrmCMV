4b85f6dd126e6de094f48161e0b383df83145a94
David Leder <davidleder98@gmail.com>
2024-08-29 22:39:34 +0200

    js for delete and edit finally working as they should... more or less

--- BookWyrmCMV/Areas/Admin/Controllers/ProductController.cs
+++ BookWyrmCMV/Areas/Admin/Controllers/ProductController.cs
@@ -114,47 +114,33 @@ namespace BookWyrmCMV.Areas.Admin.Controllers
 
 
 
-        // - - DELETE PRODUCT - -
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            ProductModel? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-
-
-
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            ProductModel? obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted successfully.";
-            return RedirectToAction("Index");
-        }
-
-
-
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll(int id)
+        public IActionResult GetAll()
         {
             List<ProductModel> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return Json(new {data = objProductList});
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if(productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImgUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Deleted successfully" });
         }
 
         #endregion
--- BookWyrmCMV/wwwroot/js/Product.js
+++ BookWyrmCMV/wwwroot/js/Product.js
@@ -7,21 +7,21 @@ function loadDataTable() {
         "ajax": { url: '/admin/product/getall' },
         "columns": [
             { data: 'title', "width": "25%" },
-            { data: 'author', "width": "25%" },
-            { data: 'isbn', "width": "20%" },
+            { data: 'author', "width": "17%" },
+            { data: 'isbn', "width": "18%" },
             { data: 'price', "width": "5%" },
             { data: 'category.name', "width": "10%" },
             {
-                "data": null,
-                "width": "15%",
-                "render": function (data, type, row) {
+                "data": 'id',
+                "width": "25%",
+                "render": function (data) {
                     return `<div class="text-center">
-                        <a href="/Admin/Product/Upsert/${row.id}" class="btn btn-success text-white" style="cursor:pointer;">
-                            <i class="bi bi-pencil-square"></i>
+                        <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-warning text-white" style="cursor:pointer;">
+                            <i class="bi bi-pencil-square"></i> Edit
                         </a>
                         &nbsp;
-                        <a onclick="Delete('/Admin/Product/Delete/${row.id}')" class="btn btn-danger text-white" style="cursor:pointer;">
-                            <i class="bi bi-trash-fill"></i>
+                        <a href="/Admin/Product/Delete/${data}" class="btn btn-danger text-white" style="cursor:pointer;">
+                            <i class="bi bi-trash-fill"></i> Delete
                         </a>
                     </div>`;
                 },
