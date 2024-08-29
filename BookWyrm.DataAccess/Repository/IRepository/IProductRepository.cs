67a147145d95d6492b365ccfdca981c9dd878240
David Leder <davidleder98@gmail.com>
2024-08-29 11:11:16 +0200

    product repositories

---
+++ BookWyrm.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookWyrm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWyrm.DataAccess.Repository.IRepository
+{
+	public interface IProductRepository : IRepository<ProductModel>
+	{
+		void Update(ProductModel obj);
+	}
+}
---
+++ BookWyrm.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using BookWyrm.DataAccess.Data;
+using BookWyrm.DataAccess.Repository.IRepository;
+using BookWyrm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWyrm.DataAccess.Repository
+{
+	public class ProductRepository : Repository<ProductModel>, IProductRepository
+	{
+		private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+		public void Update(ProductModel obj)
+		{
+			_db.Products.Update(obj);
+		}
+	}
+}
