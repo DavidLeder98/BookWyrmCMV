00f2a99f45842f39100280e97b2da9be6be44fdd
David Leder <davidleder98@gmail.com>
2024-08-29 21:22:29 +0200

    JS table works now, but delete doesn't

--- BookWyrmCMV/wwwroot/js/Product.js
+++ BookWyrmCMV/wwwroot/js/Product.js
@@ -1,5 +1,4 @@
-﻿
-$(document).ready(function (){
+﻿$(document).ready(function () {
     loadDataTable();
 });
 
@@ -7,11 +6,31 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'title', "width" : "15%" },
-            { data: 'author', "width": "15%" },
-            { data: 'isbn', "width": "15%" },
-            { data: 'price', "width": "15%" },
-            { data: 'description', "width": "15%" },
-            { data: 'category.name', "width": "15%" }]
+            { data: 'title', "width": "25%" },
+            { data: 'author', "width": "25%" },
+            { data: 'isbn', "width": "20%" },
+            { data: 'price', "width": "5%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                "data": null,
+                "width": "15%",
+                "render": function (data, type, row) {
+                    return `<div class="text-center">
+                        <a href="/Admin/Product/Upsert/${row.id}" class="btn btn-success text-white" style="cursor:pointer;">
+                            <i class="bi bi-pencil-square"></i>
+                        </a>
+                        &nbsp;
+                        <a onclick="Delete('/Admin/Product/Delete/${row.id}')" class="btn btn-danger text-white" style="cursor:pointer;">
+                            <i class="bi bi-trash-fill"></i>
+                        </a>
+                    </div>`;
+                },
+                "orderable": false
+            }
+        ],
+        "language": {
+            "emptyTable": "No data available"
+        },
+        "width": "100%"
     });
 }
