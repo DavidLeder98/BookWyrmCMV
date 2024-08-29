e9b2f63f8c0139b0fd4aaa3d16693a72c995d426
David Leder <davidleder98@gmail.com>
2024-08-29 10:14:42 +0200

    product model

---
+++ BookWyrm.Models/ProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWyrm.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string ISBN { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+        [Range(1,1000)]
+        public double Price { get; set; }
+    }
+}
