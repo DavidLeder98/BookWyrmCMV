97ef2534a477a9c0fca961cb659f24e1834bb8ea
David Leder <davidleder98@gmail.com>
2024-08-30 15:33:23 +0200

    added static roles

--- BookWyrm.Utility/StaticDetails.cs
+++ BookWyrm.Utility/StaticDetails.cs
@@ -8,5 +8,9 @@ namespace BookWyrm.Utility
 {
 	public static class StaticDetails
 	{
-	}
+		public const string Role_User_Cust = "Customer";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
 }
